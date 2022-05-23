42fcead471d455a29a2665ebf0f3908b9066b36b
nikos efthias <nikos@mugsoft.io>
2022-05-23 12:03:17 +0300

    rename pipe to _pipe

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-HTMLElement.prototype.q = function(i) { return this.querySelector(i) }
-HTMLDocument.prototype.q = function(i) { return this.querySelector(i) }
-Object.prototype.pipe = function(fn) {
-	return fn(this)
-}
+HTMLElement.prototype.q = function (i) {
+  return this.querySelector(i);
+};
+HTMLDocument.prototype.q = function (i) {
+  return this.querySelector(i);
+};
+Object.prototype._pipe = function (fn) {
+  return fn(this);
+};
